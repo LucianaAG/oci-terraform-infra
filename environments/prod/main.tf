356e254bf06af9cb80f4dba324ed87c051076361
LucyAG <lucygauto35@gmail.com>
2026-06-22 04:58:39 -0300

    feat: add prod auto tfvars and remove redundant backend file

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,3 +1,19 @@
+terraform { # configuraciones de terrraform
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+
+  cloud { # conectamos nuestro proyecto local con HCP
+    organization = "oci-devops-portfolio"
+
+    workspaces {
+      name = "oci-prod"
+    }
+  }
+}
+
 #provider "oci" { # definicion del proveedor a utilizar
  # region              = var.region
   #auth                = "SecurityToken" 
---
+++ environments/prod/prod.auto.tfvars
@@ -0,0 +1,15 @@
+subnets = { # datos de configuración para crear las subnets
+
+    dev = {
+        cidr = "172.16.2.0/24"
+        dns_label = "dev"
+        is_public = true
+    }
+
+    dev2 = {
+        cidr = "172.16.3.0/24"
+        dns_label = "dev2"
+        is_public = true
+    }
+    
+}
